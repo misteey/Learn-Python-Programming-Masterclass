7016d2c6309e702fe74eb9a6417bf90ac292c84a
Ana <lainrpgs@gmail.com>
2022-04-20 21:14:49 +0200

    Update operators.py

--- Stepping into the world of Python/operators.py
+++ Stepping into the world of Python/operators.py
@@ -10,5 +10,26 @@ print(a % b)  # 0 modulo: the remainder after integer division
 
 print()
 
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4* 12))
+print((((a + b) /3) - 4) * 12)
+print(((a + b) /3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
 for i in range(1, a // b):
   print(i)
+  
+i = 1
+print(i)
+i = 2
+print(i)
+i = 3
+print(i)
+  
+bun_price = 2.40
+money = 15
+print(money // bun_price)
