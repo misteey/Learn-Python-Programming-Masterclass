a70d3e582801888c610741d3a8c825f87bce6f79
Ana <lainrpgs@gmail.com>
2022-04-20 20:50:39 +0200

    Update strings.py

--- Stepping into the world of Python/strings.py
+++ Stepping into the world of Python/strings.py
@@ -2,3 +2,10 @@ print("Today is a good day to learn Python")
 print('Python is fun')
 print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
+print("hello" + "world")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+
+print(greeting + ' ' + name)
