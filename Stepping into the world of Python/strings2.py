b73a0006718ff312fd3d51ce7259ba4bbdc8b2b2
Ana <lainrpgs@gmail.com>
2022-04-22 14:59:38 +0200

    Update strings2.py

--- Stepping into the world of Python/strings2.py
+++ Stepping into the world of Python/strings2.py
@@ -8,3 +8,36 @@ print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+
+print(parrot[3 - 1])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+
+print(parrot[0:6]) #Norweg
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6]) + print(parrot[6:])
+
+print(parrot[:])
