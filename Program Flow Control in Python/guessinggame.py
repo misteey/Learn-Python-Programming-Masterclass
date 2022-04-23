8ba416073c6dfe66e99d95deb84b221e17f0e595
Ana <lainrpgs@gmail.com>
2022-04-23 18:03:05 +0200

    Update guessinggame.py

--- Program Flow Control in Python/guessinggame.py
+++ Program Flow Control in Python/guessinggame.py
@@ -5,7 +5,36 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 else:
     print("You got it first time")
+
+    
+# add the code to compare the trees
+tree1 = 'Kitty'
+tree2 = 'Jesse'
+if tree1 == tree2:
+    print("The trees are the same")
+else:
+    print("The trees are different")
+    
+# simple condition
+x = 5
+y = 7
+if x > y:
+    print("x is greater than y")
+elif y > x:
+    print("x is smaller than y")
+else:
+    print("x equals y")
